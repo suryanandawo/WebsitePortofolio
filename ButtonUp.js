323fb0d797c26654732efb6a21c6519d908e324c
suryanandawo <133037520+suryanandawo@users.noreply.github.com>
2024-05-05 09:57:56 +0700

    Add files via upload

---
+++ ButtonUp.js
@@ -0,0 +1,19 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
